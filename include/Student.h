fef0ae4d1334e8af1e49f684afc59894e4a10cd8
reyal04 <30076407@cc.peralta.edu>
2025-12-06 12:43:14 -0800

    Update Student.h

--- include/Student.h
+++ include/Student.h
@@ -3,30 +3,26 @@
 
 #include <string>
 #include <vector>
-#include <utility> // for std::pair
+#include <utility>
 
 class Student {
 private:
     int id;
     std::string name;
-    // list of (assignmentName, score)
-    std::vector<std::pair<std::string, double>> grades;
+    std::vector<std::pair<std::string,double>> grades;
 
 public:
     Student();
     Student(int id, const std::string& name);
 
-    // Basic getters
     int getID() const;
     std::string getName() const;
 
-    // Grade operations
     void addGrade(const std::string& assignment, double score);
     const std::vector<std::pair<std::string,double>>& getGrades() const;
     double getAverage() const;
 
-    // Display
     void display() const;
 };
 
-#endif // STUDENT_H
+#endif
